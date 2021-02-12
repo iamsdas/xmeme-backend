b3e7fb89355e97727bd587533d1b1748f7b5ccad
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-02-12 12:34:46 +0530

    use flask cors

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, Response
 from flask.globals import request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 db = SQLAlchemy(app)
 
 
