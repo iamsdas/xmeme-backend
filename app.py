c52e800497bfa903a3ccc1fda4bd8ff2ab84debd
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-02-13 23:53:55 +0530

    turn off debug mode by default

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ class SingleMemeApi(Resource):
 # only used for debugging, gunicorn used in production
 if __name__ == '__main__':
     db.create_all()
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8081)
